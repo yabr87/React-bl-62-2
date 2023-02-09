4c4924271cac44cdfbb4c68b7f87548ffdd5e541
yabr87 <108272773+yabr87@users.noreply.github.com>
2023-02-09 22:33:46 +0200

    todos

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,6 +2,16 @@ import { Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const Todo = () => {
-  return <h2>Todo</h2>;
+export const Todo = ({ id, todo, index, remove }) => {
+  return (
+    <TodoWrapper>
+      <Text textAlign="center" marginBottom="20px">
+        TODO #{index}
+      </Text>
+      <Text>{todo}</Text>
+      <DeleteButton onClick={() => remove(id)} type="button">
+        <RiDeleteBinLine size={24} />
+      </DeleteButton>
+    </TodoWrapper>
+  );
 };
